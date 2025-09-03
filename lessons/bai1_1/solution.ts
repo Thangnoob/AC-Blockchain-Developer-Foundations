2711fa6cf2ca8d7d0898199b9d960fec121e6dae
ND Thang <95998425+Thangnoob@users.noreply.github.com>
2025-09-03 21:58:49 +0700

    Completed lession 1.1 - isValidBlock

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 
 export type Block = {
   index: number;
@@ -7,7 +8,25 @@ export type Block = {
   current_hash: string;
 };
 
+export function calculateHash(
+  index: number,
+  timestamp: string,
+  transactions: any[],
+  previous_hash: string
+): string {
+  const value =
+    index + timestamp + JSON.stringify(transactions) + previous_hash;
+  return crypto.createHash("sha256").update(value).digest("hex");
+}
+
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  const expectedHash = calculateHash(
+    block.index,
+    block.timestamp,
+    block.transactions,
+    block.previous_hash
+  );
+
+  return block.current_hash == expectedHash; // Chỉnh lại logic
 }
